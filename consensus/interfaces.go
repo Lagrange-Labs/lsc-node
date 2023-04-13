5a820e2b2b38d817cf766992eff2cfbae31c3503
Cool Developer <cool199966@outlook.com>
2023-04-13 12:35:50 -0400

    wip

--- consensus/interfaces.go
+++ consensus/interfaces.go
@@ -7,6 +7,8 @@ import (
 )
 
 type storageInterface interface {
+	GetLastFinalizedBlockNumber(ctx context.Context) (uint64, error)
 	GetBlock(ctx context.Context, blockNumber uint64) (*sequencertypes.Block, error)
 	UpdateBlock(ctx context.Context, block *sequencertypes.Block) error
+	GetNodesByStatuses(ctx context.Context, statuses []sequencertypes.NodeStatus) ([]*sequencertypes.ClientNode, error)
 }
--- consensus/state.go
+++ consensus/state.go
@@ -1,22 +1,111 @@
 package consensus
 
-import "github.com/Lagrange-Labs/lagrange-node/consensus/types"
+import (
+	"context"
+	"time"
+
+	"github.com/Lagrange-Labs/lagrange-node/consensus/types"
+	sequencertypes "github.com/Lagrange-Labs/lagrange-node/sequencer/types"
+	storetypes "github.com/Lagrange-Labs/lagrange-node/store/types"
+)
+
+const CheckInterval = 500 * time.Millisecond
 
 // State handles the consensus process.
 type State struct {
 	types.RoundState
 
-	storage storageInterface
+	proposer      *types.Validator
+	storage       storageInterface
+	roundLimit    time.Duration
+	roundInterval time.Duration
 }
 
 // NewState returns a new State.
 func NewState(cfg *Config, storage storageInterface) *State {
 	return &State{
-		storage: storage,
+		proposer: &types.Validator{
+			PublicKey: cfg.ProposerPubKey,
+		},
+		storage:    storage,
+		roundLimit: time.Duration(cfg.RoundLimit),
 	}
 }
 
-// OnStart loads the first unverified block and starts the receive routine.
+// OnStart loads the first unverified block and starts the round.
 func (s *State) OnStart() error {
+	lastBlockNumber, err := s.storage.GetLastFinalizedBlockNumber(context.Background())
+	if err != nil {
+		return err
+	}
+
+	for {
+		if err := s.startRound(lastBlockNumber); err != nil {
+			return err
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), time.Duration(s.roundLimit))
+		defer cancel()
+		if err := s.processRound(ctx); err != nil {
+			// TODO handle timeout error
+			return err
+		}
+
+		lastBlockNumber++
+	}
+}
+
+func (s *State) startRound(blockNumber uint64) error {
+	nodes, err := s.storage.GetNodesByStatuses(context.Background(), []sequencertypes.NodeStatus{sequencertypes.NodeRegistered})
+	if err != nil {
+		return err
+	}
+	validatorSet := types.NewValidatorSet(s.proposer, nodes)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(s.roundLimit))
+	defer cancel()
+	block, err := s.getNextBlock(ctx, blockNumber)
+	if err != nil {
+		// TODO handle timeout error
+		return err
+	}
+
+	s.RoundState = *types.NewRoundState(validatorSet, block)
+
 	return nil
+}
+
+func (s *State) getNextBlock(ctx context.Context, blockNumber uint64) (*sequencertypes.Block, error) {
+	block, err := s.storage.GetBlock(context.Background(), blockNumber+1)
+	if err == nil || err != storetypes.ErrBlockNotFound {
+		return block, err
+	}
+	// in case the block is not found, wait for it to be added from the sequencer
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case <-time.After(CheckInterval):
+			block, err := s.storage.GetBlock(context.Background(), blockNumber+1)
+			if err != nil {
+				if err == storetypes.ErrBlockNotFound {
+					continue
+				}
+				return nil, err
+			}
+
+			return block, nil
+		}
+	}
+}
+
+func (s *State) processRound(ctx context.Context) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-time.After(s.roundInterval):
+
+		}
+	}
 }
