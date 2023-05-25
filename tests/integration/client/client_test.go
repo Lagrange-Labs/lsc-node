8faa8a1cb2e73af2b089e5bb6e6ec41f560bd435
Cool Developer <cool199966@outlook.com>
2023-05-25 10:33:45 -0400

    unblocking flow

--- tests/integration/client/client_test.go
+++ tests/integration/client/client_test.go
@@ -36,7 +36,7 @@ func (suite *ClientTestSuite) SetupTest() {
 		Chain:           "arbitrum",
 		RPCEndpoint:     "http://127.0.0.1:8545",
 		BLSPrivateKey:   "0x0642cf177a12c962938366d7c2d286f49806625831aaed8e861405bfdd1f654a",
-		ECDSAPrivateKey: "0x232d99bc62cf95c358fb496e9f820ec299f43417397cea32f9f365daf4748429",
+		ECDSAPrivateKey: "0xb126ae5e3d88007081b76024477b854ca4f808d48be1e22fe763822bc0c17cb3",
 		PullInterval:    utils.TimeDuration(2 * time.Second),
 	}
 	var err error
