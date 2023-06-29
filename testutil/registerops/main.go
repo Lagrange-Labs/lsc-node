4f9aa84cc395030a7501b440850bbc1d4eeedf25
cool-developer <51834436+cool-develope@users.noreply.github.com>
2023-06-29 17:16:43 -0400

    Added the config generation tool (#97)
    * adding the config generation tool
    
    * reflect the SC updates

--- testutil/registerops/main.go
+++ testutil/registerops/main.go
@@ -11,7 +11,7 @@ import (
 )
 
 var (
-	StakeAddress   = common.HexToAddress("0x98f07aB2d35638B79582b250C01444cEce0E517A")
+	StakeAddress   = common.HexToAddress("0xf32358f5C8FFfCF1a7bDb58b270a082abb7Ba1A6")
 	SlasherAddress = common.HexToAddress("0x6Bf0fF4eBa00E3668c0241bb1C622CDBFE55bbE0")
 	PrivateKeys    = []string{
 		"0x232d99bc62cf95c358fb496e9f820ec299f43417397cea32f9f365daf4748429",
