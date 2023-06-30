71df4277d0ba7830e29d01b411dcbbafbc4fcd66
Cool Developer <cool199966@outlook.com>
2023-06-29 20:41:48 -0400

    fix test failures

--- tests/integration/sequencer_test.go
+++ tests/integration/sequencer_test.go
@@ -48,7 +48,7 @@ func (suite *SequencerTestSuite) Test_Sequencer_Block_Generation() {
 	go suite.sequencer.Start() // nolint:errcheck
 	time.Sleep(5 * time.Second)
 
-	block, err := suite.manager.Storage.GetBlock(context.Background(), suite.sequencer.GetChainID(), 1)
+	block, err := suite.manager.Storage.GetBlock(context.Background(), suite.sequencer.GetChainID(), 5)
 	require.NoError(suite.T(), err)
 	require.NotNil(suite.T(), block)
 	require.NotNil(suite.T(), block.AggSignature)
