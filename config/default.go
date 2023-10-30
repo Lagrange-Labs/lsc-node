ffbc502ed3b4b1b0cc8eebefc8ac9aaed5ed41f3
cool-developer <51834436+cool-develope@users.noreply.github.com>
2023-10-30 10:27:12 -0400

    feat: sync the smart contract updates (#145)
    * sc updates
    
    * fix tests

--- config/default.go
+++ config/default.go
@@ -29,7 +29,6 @@ FromBlockNumber = 5
 [Governance]
 EthereumURL = "http://127.0.0.1:8545"
 PrivateKey = "0x3e17bc938ec10c865fc4e2d049902716dc0712b5b0e688b7183c16807234a84c"
-StakingSCAddress = "0xe509Dea686816C3c64ed4950dA54792a70A25239"
 CommitteeSCAddress = "0x0AEd0dC7f53CB452A34A3Fe4d6a7E4Fdd110ed0f"
 StakingCheckInterval = "2s"
 EvidenceUploadInterval = "3s"
