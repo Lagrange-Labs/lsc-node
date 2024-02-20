5f931c2773014c5a3800a70ee3da3a0cbd44abf1
Cool Developer <cool199966@outlook.com>
2024-02-20 15:40:04 -0500

    fix join tests

--- tests/integration/client_test.go
+++ tests/integration/client_test.go
@@ -29,7 +29,7 @@ func (suite *ClientTestSuite) SetupTest() {
 		Chain:           "arbitrum",
 		EthereumURL:     "http://localhost:8545",
 		BLSPrivateKey:   "0x00000000000000000000000000000000000000000000000000000000499602d4",
-		ECDSAPrivateKey: "0xb126ae5e3d88007081b76024477b854ca4f808d48be1e22fe763822bc0c17cb3",
+		ECDSAPrivateKey: "0x232d99bc62cf95c358fb496e9f820ec299f43417397cea32f9f365daf4748429",
 		PullInterval:    utils.TimeDuration(2 * time.Second),
 		BLSCurve:        "BN254",
 	}
