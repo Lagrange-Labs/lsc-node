c1b1a55362f03d21943a16f36df7566cc9fa701b
Cool Developer <cool199966@outlook.com>
2024-02-20 15:45:11 -0500

    fix

--- tests/integration/client_test.go
+++ tests/integration/client_test.go
@@ -28,8 +28,8 @@ func (suite *ClientTestSuite) SetupTest() {
 		GrpcURL:         "127.0.0.1:9090",
 		Chain:           "arbitrum",
 		EthereumURL:     "http://localhost:8545",
-		BLSPrivateKey:   "0x00000000000000000000000000000000000000000000000000000000499602d4",
-		ECDSAPrivateKey: "0x232d99bc62cf95c358fb496e9f820ec299f43417397cea32f9f365daf4748429",
+		BLSPrivateKey:   "0x00000000000000000000000000000000000000000000000000000000499602d7",
+		ECDSAPrivateKey: "0xb126ae5e3d88007081b76024477b854ca4f808d48be1e22fe763822bc0c17cb3",
 		PullInterval:    utils.TimeDuration(2 * time.Second),
 		BLSCurve:        "BN254",
 	}
