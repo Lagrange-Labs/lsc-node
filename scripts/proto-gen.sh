0687b3196f0ad6b098d1a5ce0ff6953eb3bc044a
Master Engineer <104206186+mastereng12@users.noreply.github.com>
2024-03-08 04:34:40 -0600

    feat: redesign proto schema (#263)

--- scripts/proto-gen.sh
+++ scripts/proto-gen.sh
@@ -11,7 +11,7 @@ for dir in $proto_dirs; do
     sudo rm -rf gen
     docker run --rm -v $PWD:/workspace --workdir /workspace ${protoImageName} generate --template buf.gen.yaml $file
     sudo chmod -R 775 gen
-    cp -r gen/go/${dir}/* ../$(echo "${dir}" | sed 's#^\./\([^/]*\)/.*$#\1#')/types/
+    cp -r gen/go/${dir}/* ../$(echo "${dir}" | sed 's/\(.*\)\/v\([2-9]\)/\1\/types\/v\2/; s/\(.*\)\/v1/\1\/types/')
     sudo rm -rf gen
   done
 done
