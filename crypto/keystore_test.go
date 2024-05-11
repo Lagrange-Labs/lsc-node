c1521bbe7acab1aeb1f752ba86a646fb078e3dd7
Cool Developer <cool199966@outlook.com>
2024-05-11 08:56:36 -0400

    add tests of password and keystore load

--- crypto/keystore_test.go
+++ crypto/keystore_test.go
@@ -1,33 +1,83 @@
 package crypto
 
 import (
+	"os"
 	"testing"
 
 	"github.com/Lagrange-Labs/lagrange-node/utils"
 	"github.com/stretchr/testify/require"
 )
 
-func TestKeystoreSaveAndLoad(t *testing.T) {
+func TestKeystoreFile(t *testing.T) {
 	dir := t.TempDir()
-
-	// save ECDSA key
+	passwordPath := dir + "/password"
 	ecdsaKeyPath := dir + "/ecdsa.key"
-	ecdsaPrivKey := utils.Hex2Bytes("0xb126ae5e3d88007081b76024477b854ca4f808d48be1e22fe763822bc0c17cb3")
-	err := SaveKey("ECDSA", ecdsaPrivKey, "password", ecdsaKeyPath)
-	require.NoError(t, err)
-	// load ECDSA key
-	loadedECDSAKey, err := LoadPrivateKey("ECDSA", "password", ecdsaKeyPath)
-	require.NoError(t, err)
-	require.Equal(t, ecdsaPrivKey, loadedECDSAKey)
-
-	// save BLS key
 	blsKeyPath := dir + "/bls.key"
-	blsPrivKey, err := NewBLSScheme(BN254).GenerateRandomKey()
-	require.NoError(t, err)
-	err = SaveKey("BN254", blsPrivKey, "password", blsKeyPath)
-	require.NoError(t, err)
-	// load BLS key
-	loadedBLSKey, err := LoadPrivateKey("BN254", "password", blsKeyPath)
-	require.NoError(t, err)
-	require.Equal(t, blsPrivKey, loadedBLSKey)
+
+	testCases := []struct {
+		desc     string
+		password string
+		exact    string
+	}{
+		{
+			desc:     "empty password",
+			password: "",
+			exact:    "",
+		},
+		{
+			desc:     "alphanumeric password 1",
+			password: "password",
+			exact:    "password",
+		},
+		{
+			desc:     "alphanumeric password 2",
+			password: "password123",
+			exact:    "password123",
+		},
+		{
+			desc:     "alphanumeric password 2",
+			password: "012password123",
+			exact:    "012password123",
+		},
+		{
+			desc:     "special characters password",
+			password: "!@#$%^&*()_+",
+			exact:    "!@#$%^&*()_+",
+		},
+		{
+			desc:     "whitespace password",
+			password: "  password\t \n",
+			exact:    "password",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.desc, func(t *testing.T) {
+			err := os.WriteFile(passwordPath, []byte(tc.password), 0644)
+			require.NoError(t, err)
+
+			password, err := ReadKeystorePasswordFromFile(passwordPath)
+			require.NoError(t, err)
+			require.Equal(t, tc.exact, password)
+
+			// save ECDSA key
+			ecdsaPrivKey := utils.Hex2Bytes("0xb126ae5e3d88007081b76024477b854ca4f808d48be1e22fe763822bc0c17cb3")
+			err = SaveKey("ECDSA", ecdsaPrivKey, tc.exact, ecdsaKeyPath)
+			require.NoError(t, err)
+			// load ECDSA key
+			loadedECDSAKey, err := LoadPrivateKey("ECDSA", tc.exact, ecdsaKeyPath)
+			require.NoError(t, err)
+			require.Equal(t, ecdsaPrivKey, loadedECDSAKey)
+
+			// save BLS key
+			blsPrivKey, err := NewBLSScheme(BN254).GenerateRandomKey()
+			require.NoError(t, err)
+			err = SaveKey("BN254", blsPrivKey, tc.exact, blsKeyPath)
+			require.NoError(t, err)
+			// load BLS key
+			loadedBLSKey, err := LoadPrivateKey("BN254", tc.exact, blsKeyPath)
+			require.NoError(t, err)
+			require.Equal(t, blsPrivKey, loadedBLSKey)
+		})
+	}
 }
