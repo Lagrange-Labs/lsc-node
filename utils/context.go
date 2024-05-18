e2895df62aa663578d22b01718f9e366b73e420e
mastereng12 <viktor.miller.eng@gmail.com>
2024-05-17 21:48:53 -0400

    lint

--- utils/context.go
+++ utils/context.go
@@ -12,13 +12,13 @@ const (
 
 // GetContext returns a context with a default timeout.
 func GetContext() context.Context {
-	ctx, _ := context.WithTimeout(context.Background(), DefaultContextTimeout)
+	ctx, _ := context.WithTimeout(context.Background(), DefaultContextTimeout) // nolint:govet
 	return ctx
 }
 
 // GetContextWithTimeout returns a context with a timeout.
 func GetContextWithTimeout(timeout time.Duration) context.Context {
-	ctx, _ := context.WithTimeout(context.Background(), timeout)
+	ctx, _ := context.WithTimeout(context.Background(), timeout) // nolint:govet
 	return ctx
 }
 
