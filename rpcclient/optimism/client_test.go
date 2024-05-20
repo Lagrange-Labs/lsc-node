dec0b7c8e285b19d1155dfd571be130fe9649bb9
mastereng12 <viktor.miller.eng@gmail.com>
2024-05-20 15:18:22 -0400

    fix test

--- rpcclient/optimism/client_test.go
+++ rpcclient/optimism/client_test.go
@@ -1,6 +1,7 @@
 package optimism
 
 import (
+	"sync"
 	"testing"
 	"time"
 
@@ -21,12 +22,27 @@ func TestErrorHandling(t *testing.T) {
 	require.NoError(t, err)
 
 	client.SetBeginBlockNumber(10)
+	mtx := sync.Mutex{}
+	mtx.Lock()
 	go func() {
+		defer mtx.Unlock()
+		time.Sleep(2 * time.Second)
+		client.fetcher.StopFetch()
+	}()
+	mtx.Lock()
+
+	// check if able to restart
+	client.SetBeginBlockNumber(50)
+	mtx.Unlock()
+	mtx.Lock()
+	go func() {
+		defer mtx.Unlock()
 		time.Sleep(2 * time.Second)
 		client.fetcher.Stop()
 	}()
+	mtx.Lock()
+	defer mtx.Unlock()
+	// check error propagation
 	_, err = client.NextBatch()
 	require.Error(t, err)
-	// check if able to restart
-	client.SetBeginBlockNumber(10)
 }
