6361ddd868543568ce0264df8ab419ec9954a79a
Kashish Shah <kashish@lagrange.dev>
2024-07-17 10:20:14 +0200

    implement db update to add from/to l2 blocks fields

--- store/mongodb/migrations/init.go
+++ store/mongodb/migrations/init.go
@@ -4,4 +4,5 @@ func init() {
 	// Register the migration with the migration manager
 	_ = RegisterMigration("0001_init", up_0001, down_0001)
 	_ = RegisterMigration("0002_add_operators_field", up_0002, down_0002)
+	_ = RegisterMigration("0003_add_l2_blocks_range", up_0003, down_0003)
 }
